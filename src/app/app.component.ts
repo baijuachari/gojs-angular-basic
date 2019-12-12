3e87e8eba580f9d9be55a307a526860356dcaad9
Baiju Achari <Achari_Baiju@bah.com>
2019-12-12 16:18:41 -0500

    added gojs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,7 +11,7 @@ export class AppComponent implements OnInit {
 
   ngOnInit() {
     console.log('I am in App Init');
-    // this.drawSimpleDiagram();ßßßßß
+
     this.drawSimpleDiagramAndModel();
   }
 
@@ -22,34 +22,17 @@ export class AppComponent implements OnInit {
     const myModel = $(go.Model);
     myDiagram.model = myModel;
 
+    myDiagram.nodeTemplate =
+      $(go.Node,
+        $(go.TextBlock,
+          new go.Binding('text', 'key')
+        ),
+      );
+
     myModel.nodeDataArray = [
       { key: 'Alpha'},
       { key: 'Beta'},
       { key: 'Gamma'}
     ];
   }
-  // private drawSimpleDiagram() {
-  //   const $ = go.GraphObject.make;
-  //   const myDiagram = $(go.Diagram, 'myDiagramDiv');
-
-  //   const nodeDataArray = [
-  //     {key: 'Alpha', color: 'lime'},
-  //     {key: 'Beta', color: 'cyan'}
-  //   ];
-
-  //   const linkDataArray = [
-  //     {to: 'Beta', from: 'Alpha'}
-  //   ];
-
-  //   myDiagram.model = new go.GraphLinksModel(nodeDataArray, linkDataArray);
-  //   myDiagram.nodeTemplate =
-  //     $(go.Node, 'Auto',
-  //       $(go.Shape, 'RoundedRectangle', { fill: 'white' },
-  //         new go.Binding('fill', 'color')
-  //       ),
-  //       $(go.TextBlock, 'Hello',
-  //         new go.Binding('text', 'key')
-  //       )
-  //     );
-  // }
 }
